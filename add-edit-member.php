8e30a457e0e4d36afcd23d31d6c62956a945f98e
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-09-07 20:47:22 +0200

    build: add files

---
+++ add-edit-member.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'functions.php';
+
+if (!empty($_POST)) {
+    $is_active = $_POST['is_active'] ?? '0';
+    $first_name = $_POST['first_name'] ?? '';
+    $last_name = $_POST['last_name'] ?? '';
+    $address = $_POST['address'] ?? '';
+    $tier_id = filter_input(INPUT_POST, 'tier_id', FILTER_SANITIZE_NUMBER_INT);
+    $db = connect();
+    if (empty($_POST['id'])) {
+        try {
+            $createMemberStmt =  $db->prepare('INSERT INTO members (is_active, first_name, last_name, address, tier_id) VALUES (:is_active, :first_name, :last_name, :address, :tier_id)');
+            $createMemberStmt->execute(['is_active' => $is_active, 'first_name' => $first_name, 'last_name' => $last_name, 'address' => $address, 'tier_id' => $tier_id]);
+            if ($createMemberStmt->rowCount()) {
+                $type = 'success';
+                $message = 'Member added';
+            } else {
+                $type = 'error';
+                $message = 'Member not added';
+            }
+        } catch (Exception $e) {
+            $type = 'error';
+            $message = 'Member not added: ' . $e->getMessage();
+        }
+    } else {
+        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+        try {
+            $updateMemberStmt = $db->prepare('UPDATE members SET is_active = :is_active, first_name = :first_name, last_name = :last_name, address = :address, tier_id = :tier_id WHERE id = :id');
+
+            $updateMemberStmt->execute(['is_active' => $is_active, 'first_name' => $first_name, 'last_name' => $last_name, 'address' => $address, 'tier_id' => $tier_id, 'id' => $id]);
+
+            if ($updateMemberStmt->rowCount()) {
+                $type = 'success';
+                $message = 'Member updated';
+            } else {
+                $type = 'error';
+                $message = 'Member not updated';
+            }
+        } catch (Exception $e) {
+            $type = 'error';
+            $message = 'Member not updated: ' . $e->getMessage();
+        }
+    }
+    $createMemberStmt = null;
+    $updateMemberStmt = null;
+    $db = null;
+
+    header('location:' . 'members.php?type=' . $type . '&message=' . $message);
+}
